5dffb9ea2ceb0297f1a04f2469305d98fae4c4f5
Amadi Jerry <amadijerry310@gmail.com>
2022-12-15 06:19:07 +0100

    90% completed checking of unread messages

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const markAll = document.querySelector(".mark-as-read")
 const totalNotifications = document.querySelector(".total-notifications")
 const unreadDots = document.querySelectorAll(".red-dot")
 const unreadMessageBackground = document.querySelectorAll(".unread-message")
-
+let unread = 3
 
 
 
@@ -10,4 +10,16 @@ markAll.addEventListener("click", () => {
     totalNotifications.style.display = "none"
     unreadDots.forEach(dot => dot.style.display = "none")
     unreadMessageBackground.forEach(bg => bg.style.background = "none")
+})
+
+unreadMessageBackground.forEach(bg =>{
+    bg.addEventListener("click", () => {
+        bg.style.background = "none"
+        unread--
+        if(unread == 0){
+            totalNotifications.style.display = "none"
+        }else{
+            totalNotifications.innerHTML = `<p>${unread}</p>`
+        }
+    })
 })
